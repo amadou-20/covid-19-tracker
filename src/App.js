b68a11375b4c22a11597ea264d874791b0790355
amadou-20 <ahmo300@outlook.com>
2020-12-25 22:30:30 +0100

    I get back all the countries now

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 import { MenuItem, FormControl, Select, Card, CardContent  } from '@material-ui/core';
 
 function App() {
+
+  const [countries, setCountries] = useState([
+    'USA','UK','INDIA','SENEGAL','JAPON'
+  ]);
+  // https://disease.sv/v3/covid-19/countries
+
+  useEffect(() => {
+    // async --> send a request, wait for it, do something with
+    const getCountriesData = async() => {
+    await  fetch("https://disease.sh/v3/covid-19/countries")
+       .then((reponse) => reponse.json())
+       .then((data) => {
+         const countries = data.map((country) => ({
+         name: country.country, // united states, united kingdom
+         value: country.countryInfo.iso2 // USA, UK, FR
+         }));
+
+      setCountries(countries);
+       });
+    }
+    getCountriesData();
+  }, [countries]);
 
   return (
     <div className="App">
@@ -12,10 +34,10 @@ function App() {
           variant="outlined"
           value="abc"
         >
-         <MenuItem value="worldwide">Worldwide</MenuItem>
-         <MenuItem value="worldwide">option 2</MenuItem>
-         <MenuItem value="worldwide">option 3</MenuItem>
-         <MenuItem value="worldwide">Deeeeeew</MenuItem>
+          {countries.map(country => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))
+          }
         </Select>
       </FormControl>
     </div>
