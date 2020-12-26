19abb232da0d2d63015b397b6a2674b0343c7dbc
amadou-20 <ahmo300@outlook.com>
2020-12-26 12:45:06 +0100

    I've rapped the two main div with his parent container classname = app

--- src/App.js
+++ src/App.js
@@ -37,29 +37,40 @@ function App() {
 
   return (
     <div className="app">
-      <div className="app_header">
-      <h1>COVID-19 TRACKER</h1>
-      <FormControl className="app_dropdown">
-        <Select 
-          variant="outlined" onChange={onCountryChange}
-          value={country}
-        >
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))
-          }
-        </Select>
-      </FormControl>
-    </div>
+      <div className="app_left">
+        <div className="app_header">
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className="app_dropdown">
+            <Select 
+              variant="outlined" onChange={onCountryChange}
+              value={country}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                  <MenuItem value={country.value}>{country.name}</MenuItem>
+                ))
+              }
+            </Select>
+          </FormControl>
+        </div>
 
-    <div className="app_stats">
-      <InfoBox title="Coronavirus Cases" cases={300} total={400} />
-      <InfoBox title="Recovered" cases={100} total={300} />
-      <InfoBox title="Deaths" cases={2200} total={3000} />
-    </div>
-   
-    <Map />
+        <div className="app_stats">
+          <InfoBox title="Coronavirus Cases" cases={300} total={400} />
+          <InfoBox title="Recovered" cases={100} total={300} />
+          <InfoBox title="Deaths" cases={2200} total={3000} />
+        </div>
+
+        <Map />
+      </div> 
+
+      <Card className="app_right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          { /*Table*/ }
+          <h3>Worldwide new cases</h3>
+          { /*Graph*/ }
+          </CardContent>
+      </Card>
 
 </div>
   );
