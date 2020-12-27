b30bc01c6ddf065615a4c1bf7abc9e77edba0df2
amadou-20 <ahmo300@outlook.com>
2020-12-27 18:48:50 +0100

    There is a nother sort way to sort data here

--- src/Util.js
+++ src/Util.js
@@ -1,11 +1,6 @@
 export const sortData = (data) => {
     let sortedData = [...data];
-    sortedData.sort((a, b) => {
-      if (a.cases > b.cases) {
-        return -1;
-      } else {
-        return 1;
-      }
-    });
-    return sortedData;
+
+    return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
+       
   };
